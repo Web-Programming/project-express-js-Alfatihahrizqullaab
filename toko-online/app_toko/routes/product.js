8843a6331f9939da416fcb8747d5ee77219b668e
Alfatihah Rizqullaah <alfatihahrizqullaah@gmail.com>
2025-09-29 20:11:50 +0700

    kumpul tugas 3

--- toko-online/app_toko/routes/product.js
+++ toko-online/app_toko/routes/product.js
@@ -1,19 +1,30 @@
-const express = require('express');
-const router = express.Router();
+    const express = require('express');
+    const router = express.Router();
+    const products = require("../../data/product.json");
 
+    router.get("/:id", function(req, res, next){
+        const productId = parseInt(req.params.id); // tangkap ID dari URL
+        const product = products.find(p => p.id == productId);
 
-router.get("/:id", function(req, res, next){
-    const productId = parseInt(req.params.id); // tangkap ID dari URL
-    const product = products.find(p => p.id == productId);
+        if(!product){
+            return res.status(404).send("produk tidak ditemukan")
+        }
 
-    if(!product){
-        return res.status(404).send("produk tidak ditemukan")
-    }
-
-    res.render('product-detail', {
-        title: product.name,
-        product
+        res.render('product-detail', {
+            title: product.name,
+            product
+        })
     })
-})
 
-module.exports = router;
+    router.get('/:productId/review/:reviewId', function(req, res, next){
+        const productId = req.params.productId;
+        const reviewId = req.params.reviewId;
+        // kirim kedua parameter ke view untuk ditampilkan 
+        res.render('review-detail', {
+            title: `Ulasan ${reviewId} untuk Produk ${productId}`,
+            productId: productId,
+            reviewId: reviewId
+        })
+    })
+
+    module.exports = router;
